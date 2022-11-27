977e0c9dddad07d4caa2070adfe768319e909c61
AnneKin <annekhd98@gmail.com>
2022-11-26 22:13:59 -0300

    feat: temp cart

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -7,9 +7,11 @@ export const AuthProvider = (props) => {
     const [token, setToken] = useState({})
     const [addProduct, setAddProduct] = useState([])
     const [ user, setUser] = useState({})
+    const [tempCart, setTempCart] = useState([])
+
 
 return(
-    <AuthContext.Provider value={{token, setToken, addProduct, setAddProduct, user, setUser}}>
+    <AuthContext.Provider value={{token, setToken, addProduct, setAddProduct, user, setUser, tempCart, setTempCart}}>
         {props.children}
     </AuthContext.Provider>
 )
