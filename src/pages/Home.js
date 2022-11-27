4e8244c5175c28f53183374ab90ce922f1414f25
AnneKin <annekhd98@gmail.com>
2022-11-27 17:27:51 -0300

    fix: merging

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,6 @@ export default function Home() {
 
   const navigate = useNavigate()
 
-  const navigate = useNavigate()
-
   const [dogArray, setDogArray] = useState([])
   const [catArray, setCatArray] = useState([])
 
